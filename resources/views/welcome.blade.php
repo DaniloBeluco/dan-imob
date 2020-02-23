9ae80dcc9616ae1e854edf3557c8e84e3c986d9f
Danilo Cardoso Beluco <danilobeluco80@gmail.com>
2020-02-23 19:46:32 -0300

    third

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravelasdasd
                 </div>
 
                 <div class="links">
